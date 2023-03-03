0096112e17d6e2177420a717854120bf24dbc962
Jahongir2109 <jahongirachilov004@gmail.com>
2023-03-03 16:13:30 +0500

    fixed again

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -12,16 +12,12 @@ public class DeveloperProjectFinder {
                 }
             }
         }
-//        list.sort(Comparator.comparingInt(String::length));
-//        list.sort(String::compareToIgnoreCase);
-//        list.sort(Comparator.reverseOrder());
         list.sort((o1, o2) -> {
             if (o1.length()!=o2.length()) {
                 return o2.length()-o1.length(); //overflow impossible since lengths are non-negative
             }
             return o2.compareTo(o1);
         });
-//        list.sort(Comparator.reverseOrder());
         return list;
     }
 }
